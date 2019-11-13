cddb501c037079fb20faf5db05e9449765d47b1d
Derek Gloudemans <35013478+DerekGloudemans@users.noreply.github.com>
2019-11-13 11:32:48 -0600

    Update multiprocessing_Value.py

--- multiprocessing_Value.py
+++ multiprocessing_Value.py
@@ -1,22 +1,29 @@
 """
-multiprocessing Value test
+Multiprocessing Value objects (without locks, which are optionally implemented
+so this code is not really safe). The main takeaway is that the multiprocessing 
+Value defined outside of the worker functions is shared between each, whereas the
+mp value defined within the processes is not, even though it has the same name 
+(defined in different namespaces)
 """
 
 import multiprocessing as mp
 import time
 
-def worker():
+def worker(test2):
     test = mp.Value('i',10)
+    test2.value += 200
     time.sleep(5)
-    print("Value is {}".format(test.value))
-def worker2():
+    print("Worker 1: value is {}, shared value is {}".format(test.value,test2.value))
+
+def worker2(test2):
     test = mp.Value('i',20)
     time.sleep(5)
-    print("Value is {}".format(test.value))
+    print("Worker 2: value is {}, shared value is {}".format(test.value,test2.value))
 
 if __name__ == "__main__":
-    p = mp.Process(target = worker)
-    p2 = mp.Process(target = worker2)
+    test2 = mp.Value('i',30)
+    p = mp.Process(target = worker, args = (test2,))
+    p2 = mp.Process(target = worker2, args = (test2,))
     
     p.start()
     p2.start()
@@ -24,4 +31,6 @@ if __name__ == "__main__":
     p.join()
     p2.join()
     
-    
+    # output:
+    # Worker 1: value is 10, shared value is 230
+    # Worker 2: value is 20, shared value is 230
